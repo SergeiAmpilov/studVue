bd243d58235ae8a9664d47cb06a108528662f87c
Sergei Ampilov <sergei.ampilov@gmail.com>
2020-07-21 20:52:35 +0300

    vue2

--- 1/script.js
+++ 1/script.js
@@ -1,11 +1,18 @@
 var appV = new Vue({
     el: '#app',
     data: {
-        message: 'Привет, Vue!'
+        message: 'Привет, Vue!',
+        isOk: true,
+        str: 'My name is Viachestas',
     },
     methods: {
         onMyInput: function (event) {
             this.message = event.target.value;
+        },
+        sayHello: function (e) {
+            console.log(e);
+            return "Hello Sergei";
         }
+
     }
 });
